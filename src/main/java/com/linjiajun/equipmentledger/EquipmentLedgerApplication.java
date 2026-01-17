7f3d551a45e9f507c8a118794800c38387e215be
HypoCodeEmia <2198342394@qq.com>
2026-01-17 10:02:18 +0800

    build(): 初始化

---
+++ src/main/java/com/linjiajun/equipmentledger/EquipmentLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.linjiajun.equipmentledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentLedgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linjiajun/equipmentledger/EquipmentLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linjiajun.equipmentledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentLedgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
